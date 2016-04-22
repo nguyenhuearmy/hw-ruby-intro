3cbf6301a9aeb98dd1e1bcb7df1f0d38658d476e
nguyenhuearmy <huearmynguyen@gmail.com>
2016-04-22 15:31:29 +0000

    part3_spec

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -23,15 +23,26 @@ def hello(name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[a-z&&[^aeiou]]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/
+    s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    def initialize(isbn, price)
+        raise ArgumentError if isbn.empty?
+        raise ArgumentError if price <= 0
+        @isbn = isbn
+        @price = price
+    end
+    attr_accessor :isbn
+    attr_accessor :price
+    def price_as_string
+        "$#{sprintf("%.2f", @price)}"
+    end
 end
